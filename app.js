0030472b2351911307bdbfff75265d8c29701031
Jason Lam <jaylam111@gmail.com>
2018-05-17 02:28:47 -0400

    added main comp, index, gitignore

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ class Square extends React.Component {
       value: this.props.value
     };
 	}
-
   render() {
     return (
 			<div> Square
@@ -27,3 +26,41 @@ class Dice extends React.Component {
 		)
 	}
 }
+
+class Main extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			rollDisabled: null,
+			endDisabled: null,
+			sum: 45,
+			dice1: null,
+			dice2: null,
+			diceSum: 0
+		};
+	}
+	render() {
+		return (
+			<div>
+				<div id="board-container">
+
+					<div id="main" />
+
+					<Square value={1} />
+					<Square value={2} />
+					<Square value={3} />
+					<Square value={4} />
+					<Square value={5} />
+					<Square value={6} />
+					<Square value={7} />
+					<Square value={8} />
+					<Square value={9} />
+				</div>
+			</div>
+		)}
+
+}
+
+ReactDOM.render( <Main/>,
+	document.getElementById('container')
+);
